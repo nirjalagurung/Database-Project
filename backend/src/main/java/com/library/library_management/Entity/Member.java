f591d99505dc1f43308bc07a49ec2a642f3852f0
nirjalagurung <166579101+nirjalagurung@users.noreply.github.com>
2025-04-17 01:21:18 -0400

    Delete backend directory

--- backend/src/main/java/com/library/library_management/Entity/Member.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.library.library_management.Entity;
-
-public class Member {
-    @Id
-    private Long id;
-    private String name;
-    private String borrowHist;
-}
--- backend/src/main/java/com/library/library_management/Repositories/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.library.library_management.Repositories;
-
-import com.library.management.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, String> {
-}
-
